b0120089a225654b42c3c00292f899acd8a002be
Roelof Reitsma <roelof.reitsma@ogd.nl>
2013-10-18 09:30:52 +0200

    Set correct puma configuration

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,5 +1,6 @@
+directory '/var/www/user_registration/current'
 environment 'production'
 daemonize true
-pidfile '/var/lib/www/user_registration/current/tmp/pids/puma.pid'
+pidfile '/var/www/user_registration/current/tmp/pids/puma.pid'
 stdout_redirect '/var/log/user_registration/log/stdout.log', '/var/log/user_registration/stderr.log', true
 bind 'unix:///var/run/user_registration.sock'
