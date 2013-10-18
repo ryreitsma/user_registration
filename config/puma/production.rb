8ad191aa575b8c1f961bb4f7291c96d2f5791df2
Roelof Reitsma <roelof.reitsma@ogd.nl>
2013-10-18 11:17:22 +0200

    Fix incorrect log path for puma server

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,7 +1,7 @@
 directory '/var/www/user_registration/current'
 environment 'production'
 daemonize true
-stdout_redirect '/var/log/user_registration/log/puma.log', '/var/log/user_registration/puma_error.log', true
+stdout_redirect '/var/log/user_registration/puma_out.log', '/var/log/user_registration/puma_error.log', true
 bind 'unix:///var/run/user_registration/puma.sock'
 state_path '/var/run/user_registration/puma.state'
 pidfile '/var/run/user_registration/puma.pid'
