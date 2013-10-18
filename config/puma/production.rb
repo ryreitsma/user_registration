8c5c28512b232e5e2c8a3b280fd0cbe08db919b5
Roelof Reitsma <roelof.reitsma@ogd.nl>
2013-10-18 11:00:22 +0200

    Another attempt to fix puma config for deployment

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,6 +1,7 @@
 directory '/var/www/user_registration/current'
 environment 'production'
 daemonize true
-pidfile '/var/www/user_registration/current/tmp/pids/puma.pid'
-stdout_redirect '/var/log/user_registration/log/stdout.log', '/var/log/user_registration/stderr.log', true
-bind 'unix:///var/run/user_registration/unix.sock'
+stdout_redirect '/var/log/user_registration/log/puma.log', '/var/log/user_registration/puma_error.log', true
+bind 'unix:///var/run/user_registration/puma.sock'
+state_path '/var/run/user_registration/puma.state'
+pidfile '/var/run/user_registration/puma.pid'
