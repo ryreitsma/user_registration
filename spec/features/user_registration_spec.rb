06fe18aca1126e1be1b8e61e750b4e6cc3009e46
Rene van den Berg <rene.vandenberg@ogd.nl>
2013-10-24 14:57:34 +0200

    Bring acceptance tests up to date with latest translations

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -1,56 +1,43 @@
 require "capybara_spec_helper"
 
 feature "Registering for the Coconut App", js: true do
-  scenario "Registering without an e-mail address is not allowed" do
-    expected_error = "email can't be blank"
-
-    visit "/"
-
-    fill_in('Uw naam', with: 'henk')
-    expect(page).to have_no_content(expected_error)
-
-    click_button 'Aanmelden'
-
-    expect(page).to have_content(expected_error)
-  end
-
   scenario "Registering with an invalid e-mail address is not allowed" do
-    expected_error = "email is not valid"
+    expected_error = I18n.t('user.email') + " " + I18n.t('errors.messages.not_matching')
 
     visit "/"
 
-    fill_in('Uw naam', with: 'henk')
-    fill_in('Uw e-mail adres', with: 'henk')
+    fill_in(I18n.t('user.form.placeholders.name'), with: 'henk')
+    fill_in(I18n.t('user.form.placeholders.email'), with: 'henk')
     expect(page).to have_no_content(expected_error)
 
-    click_button 'Aanmelden'
+    click_button I18n.t('user.form.submit')
 
     expect(page).to have_content(expected_error)
   end
 
   scenario "Registering without a name is not allowed" do
-    expected_error = "name can't be blank"
+    expected_error = I18n.t('user.name') + " " + I18n.t('errors.messages.blank')
 
     visit "/"
 
-    fill_in('Uw e-mail adres', with: 'henk@ogd.nl')
+    fill_in(I18n.t('user.form.placeholders.email'), with: 'henk@ogd.nl')
     expect(page).to have_no_content(expected_error)
 
-    click_button 'Aanmelden'
+    click_button I18n.t('user.form.submit')
 
     expect(page).to have_content(expected_error)
   end
 
   scenario "Registering with valid information" do
-    expected_feedback = "We hebben uw registratie ontvangen"
+    expected_feedback = I18n.t('content.feedback')
 
     visit "/"
 
-    fill_in('Uw naam', with: 'henk')
-    fill_in('Uw e-mail adres', with: 'henk@ogd.nl')
+    fill_in(I18n.t('user.form.placeholders.name'), with: 'henk')
+    fill_in(I18n.t('user.form.placeholders.email'), with: 'henk@ogd.nl')
     expect(page).to have_no_content(expected_feedback)
 
-    click_button 'Aanmelden'
+    click_button I18n.t('user.form.submit')
 
     expect(page).to have_content(expected_feedback)
   end
--- spec/integration/spam_protection_spec.rb
+++ spec/integration/spam_protection_spec.rb
@@ -4,23 +4,23 @@ feature "Spam protection", js: true do
   include Capybara::DSL
 
   scenario "Registering with valid information" do
-    expected_feedback = "We hebben uw registratie ontvangen"
+    expected_feedback = I18n.t('content.feedback')
 
     visit "/"
 
-    fill_in('Uw naam', with: 'henk')
-    fill_in('Uw e-mail adres', with: 'henk@ogd.nl')
+    fill_in(I18n.t('user.form.placeholders.name'), with: 'henk')
+    fill_in(I18n.t('user.form.placeholders.email'), with: 'henk@ogd.nl')
     expect(page).to have_no_content(expected_feedback)
 
     expect {
-      click_button 'Aanmelden'
+      click_button I18n.t('user.form.submit')
 
       expect(page).to have_content(expected_feedback)
     }.to change { User.count }.by(1)
   end
 
   scenario "Attempting to use the form for spam does not work" do
-    expected_feedback = "We hebben uw registratie ontvangen"
+    expected_feedback = I18n.t('content.feedback')
 
     visit "/"
 
@@ -28,14 +28,14 @@ feature "Spam protection", js: true do
       jQuery('input').show();
     SCRIPT
 
-    fill_in('Uw naam', with: 'henk')
-    fill_in('Uw e-mail adres', with: 'henk@ogd.nl')
+    fill_in(I18n.t('user.form.placeholders.name'), with: 'henk')
+    fill_in(I18n.t('user.form.placeholders.email'), with: 'henk@ogd.nl')
     fill_in(Rails.application.config.honeypot_field_name, with: 'henk')
 
     expect(page).to have_no_content(expected_feedback)
 
     expect {
-      click_button 'Aanmelden'
+      click_button I18n.t('user.form.submit')
 
       # IMPORTANT: we want the app to fake a success
       expect(page).to have_content(expected_feedback)
